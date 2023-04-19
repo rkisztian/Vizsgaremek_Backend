8aba581906d2bda55cb3370fbc6aa742bb0b2151
Petrik Diák <diak@petrik.hu>
2023-04-19 09:59:42 +0200

    gyakorlat doksi elkészítve

--- src/gyakorlatok/dto/create-gyakorlatok.dto.ts
+++ src/gyakorlatok/dto/create-gyakorlatok.dto.ts
@@ -1,21 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export default class CreateGyakorlatokDto {
   
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'A gyakorlat megnevezése.',
+    type: 'string',
+  })
   name: string;
 
-
+  @ApiProperty({
+    description: 'A média megnevezése.',
+    type: 'string',
+  })
   media: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'A gyakorlat leírása.',
+    type: 'string',
+  })
   description: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'Az edzésnap megnevezése.',
+    type: 'string',
+  })
   edzesnapok: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'Az izomcsoport megnevezése.',
+    type: 'string',
+  })
   izopmcsoport: string;
 }
--- src/gyakorlatok/gyakorlatok.controller.ts
+++ src/gyakorlatok/gyakorlatok.controller.ts
@@ -2,17 +2,22 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { GyakorlatokService } from './gyakorlatok.service';
 import { UpdateGyakorlatokDto } from './dto/update-gyakorlatok.dto';
 import CreateGyakorlatokDto from './dto/create-gyakorlatok.dto';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 
 @Controller('gyakorlatok')
 export class GyakorlatokController {
   constructor(private readonly gyakorlatokService: GyakorlatokService) {}
 
   @Post('create')
+  @ApiOperation({ description: 'Létrehoz egy gyakorlatot.' })
   create(@Body() createGyakorlatokDto: CreateGyakorlatokDto) {
     return this.gyakorlatokService.create(createGyakorlatokDto);
   }
 
   @Post(':gyakorlatid/giveto/napid')
+  @ApiOperation({ description: 'Hozzáadja a gyakorlat id-t a nap id-hoz' })
+  @ApiParam({ name: 'gyakorlatid', description: 'Gyakorlat azonosítója.' })
+  @ApiParam({ name: 'napid', description: 'Napnak az azonosítója.' })
   gyakorlatAddtoEdzesNap(
     @Param('gyakorlatid') gyakorlatid : number,
     @Param('napid') napid : number,
@@ -21,21 +26,31 @@ export class GyakorlatokController {
   }
 
   @Get('list')
+  @ApiOperation({ description: 'Visszaadja az összes gyakorlatot' })
   findAll() {
     return this.gyakorlatokService.findAll();
   }
 
   @Get('search/:id')
+  @ApiOperation({ description: 'Visszaadja a megadott gyakorlat adatait.' })
+  @ApiParam({ name: 'id', description: 'Gyakorlat azonosítója.' })
   findOne(@Param('id') id: string) {
     return this.gyakorlatokService.findOne(+id);
   }
 
   @Patch('update/:id')
-  update(@Param('id') id: string, @Body() updateGyakorlatokDto: UpdateGyakorlatokDto) {
+  @ApiOperation({ description: 'Módosítja a megadott gyakorlat adatait.' })
+  @ApiParam({ name: 'id', description: 'Gyakorlat azonosítója.' })
+  update(
+    @Param('id') id: string,
+    @Body() updateGyakorlatokDto: UpdateGyakorlatokDto,
+  ) {
     return this.gyakorlatokService.update(+id, updateGyakorlatokDto);
   }
 
   @Delete('delete/:id')
+  @ApiOperation({ description: 'Törölje a megadott gyakorlat adatait.' })
+  @ApiParam({ name: 'id', description: 'Gyakorlat azonosítója.' })
   remove(@Param('id') id: string) {
     return this.gyakorlatokService.remove(+id);
   }
