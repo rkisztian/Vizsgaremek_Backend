f01a9bc0041f1f48b2d4c7248ffc9b94950c88f1
Petrik Diák <diak@petrik.hu>
2023-04-19 10:08:21 +0200

    user doksi elkészítve

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request }
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
@@ -10,11 +11,14 @@ export class UserController {
 
 
   @Get('list')
+  @ApiOperation({ description: 'Visszaadja az összes felhasználót.' })
   findAll() {
     return this.userService.findAll();
   }
 
   @Get('search/:id')
+  @ApiOperation({ description: 'Visszaadja a megadott felhasználó adatait.' })
+  @ApiParam({ name: 'id', description: 'Felhasználó azonosítója.' })
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
@@ -26,11 +30,15 @@ export class UserController {
   }
 
   @Patch('update/:id')
+  @ApiOperation({ description: 'Módosítja a megadott felhasználó adatait.' })
+  @ApiParam({ name: 'id', description: 'Felhasználó azonosítója.' })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
   @Delete('delete/:id')
+  @ApiOperation({ description: 'Törli a megadott felhasználó adatait.' })
+  @ApiParam({ name: 'id', description: 'Felhasználó azonosítója.' })
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
   }
