3f492e3fc8ee3e34c319aa329d90bbe6462ed2cc
Petrik Diák <diak@petrik.hu>
2023-04-19 09:32:28 +0200

    edzésterv doksi elkészítve

--- src/edzesterv/dto/createedzesterv.dto.ts
+++ src/edzesterv/dto/createedzesterv.dto.ts
@@ -1,12 +1,19 @@
-//Validációt!!!
-
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmpty, IsNotEmpty, IsString } from 'class-validator';
 
 export default class CreateEdzestervDto {
   @IsNotEmpty({ message: 'Nem lehet üres a mező' })
   @IsString()
+  @ApiProperty({
+    description: 'Az edésterv megnevezése.',
+    type: 'string',
+  })
   name: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'Az edzésnap megnevezése.',
+    type: 'string',
+  })
   edzesnapok: string;
 }
--- src/edzesterv/edzesterv.controller.ts
+++ src/edzesterv/edzesterv.controller.ts
@@ -10,6 +10,7 @@ import {
   Delete,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 import CreateEdzestervDto from './dto/createedzesterv.dto';
 import UpdateEdzestervDto from './dto/updateedzesterv.dto';
 import { EdzestervService } from './edzesterv.service';
@@ -19,21 +20,27 @@ export class EdzestervController {
   constructor(private edzestervService: EdzestervService) {}
 
   @Post('createEdzester')
+  @ApiOperation({ description: 'Létrehoz egy edzéstervet.' })
   create(@Body() createEdzestervDto: CreateEdzestervDto) {
     return this.edzestervService.create(createEdzestervDto);
   }
 
   @Get('list')
+  @ApiOperation({ description: 'Visszaadja az összes edzéstervet.' })
   findAll() {
     return this.edzestervService.findAll();
   }
 
   @Get('search/:id')
+  @ApiOperation({ description: 'Visszaadja a megadott edzésterv adatait.' })
+  @ApiParam({ name: 'id', description: 'Edzésterv azonosítója.' })
   findOne(@Param('id') id: string) {
     return this.edzestervService.findOne(+id);
   }
 
   @Patch('update/:id')
+  @ApiOperation({ description: 'Módosítja a megadott edzésterv adatait.' })
+  @ApiParam({ name: 'id', description: 'Edzésterv azonosítója.' })
   update(
     @Param('id') id: string,
     @Body() updateEdzestervDto: UpdateEdzestervDto,
@@ -42,6 +49,8 @@ export class EdzestervController {
   }
 
   @Delete('delete/:id')
+  @ApiOperation({ description: 'Törli a megadott edzésterv adatait.' })
+  @ApiParam({ name: 'id', description: 'Edzésterv azonosítója.' })
   remove(@Param('id') id: string) {
     return this.edzestervService.remove(+id);
   }
