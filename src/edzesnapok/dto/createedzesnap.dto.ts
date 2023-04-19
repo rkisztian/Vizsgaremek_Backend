54fbb756841296086b3c89d0bd3ff27bc3f394bf
Petrik Diák <diak@petrik.hu>
2023-04-19 09:25:54 +0200

    edzésnap doksi elkészítve

--- src/edzesnapok/dto/createedzesnap.dto.ts
+++ src/edzesnapok/dto/createedzesnap.dto.ts
@@ -1,13 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmpty, IsNotEmpty, IsString } from 'class-validator';
 
 export default class CreateEdzesNapDto {
+  @ApiProperty({
+    description: 'Az edzéstervnek az ID-ja. Egyedi generált ID',
+    type: 'integer',
+  })
   edzestervId: number;
 
   @IsString()
   @IsNotEmpty({})
+  @ApiProperty({
+    description: 'Az adott edzésnap megadása.',
+    type: 'string',
+  })
   name: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Az adott edzésterv megadása.',
+    type: 'string',
+  })
   edzesterv: string;
 }
--- src/edzesnapok/edzesnapok.controller.ts
+++ src/edzesnapok/edzesnapok.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 import CreateEdzesNapDto from './dto/createedzesnap.dto';
 import { UpdateEdzesNapDto } from './dto/updateedzesnap.dto';
 import { EdzesnapokService } from './edzesnapok.service';
@@ -8,38 +17,56 @@ export class EdzesnapokController {
   constructor(private readonly edzesnapService: EdzesnapokService) {}
 
   @Post('edzesnapcreate')
+  @ApiOperation({ description: 'Új edzésnapot hozz létre.' })
   create(@Body() createedzesNap: CreateEdzesNapDto) {
     return this.edzesnapService.create(createedzesNap);
   }
 
   @Post(':napid/giveto/edzestervid')
+  @ApiOperation({
+    description: 'Hozzáadja az edzésnap ID-ját az edzéstervhez.',
+  })
+  @ApiParam({
+    name: 'napid',
+    description: 'Napnak az azonosítója.',
+  })
+  @ApiParam({
+    name: 'edzestervid',
+    description: 'Az edzésterv azonosítója.',
+  })
   napAddtoEdzesterv(
     @Param('napid') napid: number,
-    @Param('edzestervid') edzestervid : number,
+    @Param('edzestervid') edzestervid: number,
   ) {
     return this.edzesnapService.napAddtoEdzesterv(napid, edzestervid);
   }
 
   @Get('list')
+  @ApiOperation({ description: 'Listázza az összes edzésnapot.' })
   findALl() {
     return this.edzesnapService.findAll();
   }
 
   @Get('search/:id')
+  @ApiOperation({ description: 'Visszaadja a megadott edzésnap adatait.' })
+  @ApiParam({ name: 'id', description: 'Edzésnap azonosítója.' })
   findOne(@Param('id') id: string) {
     return this.edzesnapService.findOne(+id);
   }
-  
+
   @Patch('update/:id')
+  @ApiOperation({ description: 'Módosítja a megadott edzésnap adatait.' })
+  @ApiParam({ name: 'id', description: 'Edzésnap azonosítója.' })
   update(
     @Param('id') id: string,
     @Body() updateedzesNapdto: UpdateEdzesNapDto,
   ) {
     return this.edzesnapService.update(+id, updateedzesNapdto);
   }
-  
 
   @Delete('delete/:id')
+  @ApiOperation({ description: 'Törli a megadott edzésnap adatait.' })
+  @ApiParam({ name: 'id', description: 'Edzésnap azonosítója.' })
   remove(@Param('id') id: string) {
     return this.edzesnapService.remove(+id);
   }
