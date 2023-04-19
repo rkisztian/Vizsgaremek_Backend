f84c59cc9323673c7fc5e42980442d273700b269
Petrik Diák <diak@petrik.hu>
2023-04-19 10:05:09 +0200

    izomcsoport doksi elkészítve

--- src/izomcsoportok/dto/create-izomcsoportok.dto.ts
+++ src/izomcsoportok/dto/create-izomcsoportok.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export default class CreateIzomcsoportokDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Az izomcsoport megnevezése.',
+    type: 'string',
+  })
   name: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'A gyakolrat megnevezése.',
+    type: 'string',
+  })
   gyakorlat: string;
 }
--- src/izomcsoportok/izomcsoportok.controller.ts
+++ src/izomcsoportok/izomcsoportok.controller.ts
@@ -11,17 +11,30 @@ import { IzomcsoportokService } from './izomcsoportok.service';
 
 import { UpdateIzomcsoportokDto } from './dto/update-izomcsoportok.dto';
 import CreateIzomcsoportokDto from './dto/create-izomcsoportok.dto';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 
 @Controller('izomcsoportok')
 export class IzomcsoportokController {
   constructor(private readonly izomcsoportokService: IzomcsoportokService) {}
 
   @Post('create')
+  @ApiOperation({ description: 'Létrehoz egy izomcsoportot.' })
   create(@Body() createIzomcsoportokDto: CreateIzomcsoportokDto) {
     return this.izomcsoportokService.create(createIzomcsoportokDto);
   }
 
   @Post(':izomid/giveto/gyakorlatid')
+  @ApiOperation({
+    description: 'Hozzáadja az izomcsoport ID-ját a gyakorlathoz.',
+  })
+  @ApiParam({
+    name: 'izomid',
+    description: 'Izomcsoportnak az azonosítója.',
+  })
+  @ApiParam({
+    name: 'gyakorlatid',
+    description: 'A gyakorlat azonosítója.',
+  })
   izomcsoportAddToGyakorlat(
     @Param('izomid') izomid : number,
     @Param('gyakorlatid') gyakorlatid : number,
@@ -30,16 +43,21 @@ export class IzomcsoportokController {
   }
 
   @Get('list')
+  @ApiOperation({ description: 'Listázza az összes izomcsoportot.' })
   findAll() {
     return this.izomcsoportokService.findAll();
   }
 
   @Get('search/:id')
+  @ApiOperation({ description: 'Visszaadja a megadott izomcsoport adatait.' })
+  @ApiParam({ name: 'id', description: 'Izomcsoport azonosítója.' })
   findOne(@Param('id') id: string) {
     return this.izomcsoportokService.findOne(+id);
   }
 
   @Patch('update/:id')
+  @ApiOperation({ description: 'Módosítja a megadott izomcsoport adatait.' })
+  @ApiParam({ name: 'id', description: 'Izomcsoport azonosítója.' })
   update(
     @Param('id') id: string,
     @Body() updateIzomcsoportokDto: UpdateIzomcsoportokDto,
@@ -48,6 +66,8 @@ export class IzomcsoportokController {
   }
 
   @Delete('delete/:id')
+  @ApiOperation({ description: 'Törli a megadott izomcsoport adatait.' })
+  @ApiParam({ name: 'id', description: 'Izomcsoport azonosítója.' })
   remove(@Param('id') id: string) {
     return this.izomcsoportokService.remove(+id);
   }
